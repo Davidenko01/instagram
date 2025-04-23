f461315fd3bb79442596f4d6f1a6dbf694aef9ae
Davor Kissner <142751416+Davidenko01@users.noreply.github.com>
2025-04-23 14:59:13 -0300

    InstaFAI

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://inspired-prawn-51.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ providers/ClerkAndConvexProviders.tsx
@@ -0,0 +1,26 @@
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+import { ClerkLoaded, ClerkProvider, useAuth } from "@clerk/clerk-expo";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+    unsavedChangesWarning: false,
+});
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+if (!publishableKey) {
+    throw new Error(
+        "Missing Publishable Key! Please set the EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    );
+}
+
+export default function ClerkAndConvexProviders({children}: {children: React.ReactNode}) {
+  return (
+    <ClerkProvider publishableKey = {publishableKey} tokenCache={tokenCache}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <ClerkLoaded>{children}</ClerkLoaded>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  )
+}
